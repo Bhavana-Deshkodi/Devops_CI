c72d2e9a6fec9dd822b0ae914a5e6e923d403e72
Bhavana_Deshkodi <bhavanaperaje126@gmail.com>
2026-03-27 09:54:04 +0530

    Initial commit of HelloMaven project

---
+++ src/main/java/project2/p2/App.java
@@ -0,0 +1,13 @@
+package project2.p2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
